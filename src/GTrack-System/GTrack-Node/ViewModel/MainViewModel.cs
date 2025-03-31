34b0e9cceb7ea6c0d71f91611a8de9196313f88e
Unknown Source <165537535+unkn-s0urc3@users.noreply.github.com>
2025-03-31 12:38:22 +0700

    Change MVVM library, change variables, change commands, add comments

--- src/GTrack-System/GTrack-Node/ViewModel/MainViewModel.cs
+++ src/GTrack-System/GTrack-Node/ViewModel/MainViewModel.cs
@@ -1,20 +1,36 @@
-﻿using System.Windows.Controls;
+﻿using MvvmCross.ViewModels;
+using System.Windows.Controls;
 using GTrack_Node.View.UserSystemControl;
-using ReactiveUI;
 
 namespace GTrack_Node.ViewModel
 {
-    public class MainViewModel : ReactiveObject
+    // ViewModel for the main window of the GTrack Node application
+    public class MainViewModel : MvxViewModel
     {
-        public string Title { get; set; }
+        // Property for the title of the application window
+        private string _title;
+        public string Title
+        {
+            get => _title; // Getter for Title
+            set => SetProperty(ref _title, value); // Setter for Title (with property change notification)
+        }
 
-        public UserControl Content { get; set; }
+        // Property for the content (UI element) displayed in the main window
+        private UserControl _content;
+        public UserControl Content
+        {
+            get => _content; // Getter for Content
+            set => SetProperty(ref _content, value); // Setter for Content (with property change notification)
+        }
 
-
+        // Constructor that sets the initial state of the ViewModel
         public MainViewModel()
         {
+            // Initializing the title of the application
             Title = "GTrack-Node";
-            Content = new MainUserControl();
+
+            // Initializing the content to be displayed in the main window (assigning a UserControl)
+            Content = new MainUserControl(); // Setting the content to the custom UserControl
         }
     }
 }
