61ecc613a2513b7399515adf38899d00e8da8bb2
Unknown Source <165537535+unkn-s0urc3@users.noreply.github.com>
2025-03-31 12:31:24 +0700

    Change MVVM library, add comments

--- src/GTrack-System/GTrack-Control/ViewModel/MainViewModel.cs
+++ src/GTrack-System/GTrack-Control/ViewModel/MainViewModel.cs
@@ -1,29 +1,33 @@
-﻿using System.Windows.Controls;
+﻿using MvvmCross.ViewModels;
+using System.Windows.Controls;
 using GTrack_Control.View.UserSystemControl;
-using ReactiveUI;
 
 namespace GTrack_Control.ViewModel
 {
-    public class MainViewModel : ReactiveObject
+    // Main ViewModel for the application, responsible for managing the title and content of the main view.
+    public class MainViewModel : MvxViewModel
     {
+        // Property to hold the title of the window or main screen
         private string _title;
         public string Title
         {
             get => _title;
-            set => this.RaiseAndSetIfChanged(ref _title, value);
+            set => SetProperty(ref _title, value); // Sets the property value and notifies any changes
         }
 
+        // Property to hold the content (UserControl) to be displayed on the main screen
         private UserControl _content;
         public UserControl Content
         {
             get => _content;
-            set => this.RaiseAndSetIfChanged(ref _content, value);
+            set => SetProperty(ref _content, value); // Sets the property value and notifies any changes
         }
 
+        // Constructor where the default values for Title and Content are set
         public MainViewModel()
         {
-            Title = "GTrack-Control";
-            Content = new MainUserControl();
+            Title = "GTrack-Control"; // Set the default title for the main window
+            Content = new MainUserControl(); // Set the default content (a UserControl, likely the main user interface)
         }
     }
 }
